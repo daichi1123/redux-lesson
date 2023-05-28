00913c90d9327e3dc73c0dc4f644e4ad7bb6f514
daichi1123 <daichitokumasu423@gmail.com>
2023-05-28 16:39:29 +0900

    dispatchでコンソール出力するまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { createStore, Reducer } from 'redux';
+
+const initialState = 0;
+
+// Action どんな変化が起きるかを定義する
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// Reducer やってきたアクションに応じてどう変化するかを定義する
+const counterReducer = (state: number = initialState, action: any) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state -1;
+    default:
+      return state;
+  }
+}
+
+// Store アプリケーションの状態を保持する
+let store = createStore(counterReducer);
+
+// コンソールに表示する
+store.subscribe(() => console.log(store.getState()));
+
+// actionをstoreにdispatchする
+store.dispatch(increment());
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Hello Redux</h1>
     </div>
   );
 }
